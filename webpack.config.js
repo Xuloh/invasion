60c9227fd92c518d9e8f98d71d6b83e5f20bef07
Pacôme <pacome.lehoux@laposte.net>
2019-07-08 19:03:22 +0200

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const SRC_DIR = path.resolve(__dirname, "src");
+const BUNDLE_NAME = "invasion.bundle.js";
+
+const loaders = {
+    babelLoader: {
+        loader: "babel-loader",
+        options: {
+            presets: [
+                "@babel/preset-env",
+                //"@babel/preset-react"
+            ],
+            cacheDirectory: true
+        }
+    },
+    styleLoader: {
+        loader: "style-loader",
+        options: {
+            singleton: true
+        }
+    },
+    cssLoader: "css-loader",
+    /*sassLoader: {
+        loader: "sass-loader",
+        options: {
+            implementation: require("sass")
+        }
+    },*/
+    fileLoader: "file-loader"
+};
+
+module.exports = {
+    entry: path.join(SRC_DIR, "invasion.js"),
+    output: {
+        path: DIST_DIR,
+        filename: BUNDLE_NAME
+    },
+    target: "web",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: loaders.babelLoader
+            },
+            {
+                test: /\.css/,
+                use: [
+                    loaders.styleLoader,
+                    loaders.cssLoader
+                ]
+            },
+            /*{
+                test: /\.scss/,
+                exclude: /node_modules/,
+                use: [
+                    loaders.styleLoader,
+                    loaders.cssLoader,
+                    loaders.sassLoader
+                ]
+            },
+            {
+                test: /\.ttf/,
+                exclude: /node_modules/,
+                use: loaders.fileLoader
+            }*/
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Invasion"
+        })
+    ],
+    devServer: {
+        index: "index.html",
+        contentBase: DIST_DIR,
+        port: 8080,
+        compress: true,
+        historyApiFallback: true
+    }
+};
