4c75e0e743bedc031cfa88123be6ec9811bf0afa
Xuloh <pacome.lehoux@laposte.net>
2019-07-10 17:09:56 +0200

    added material-ui and replaced vanilla button with material button in menu

--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 export default class Menu extends React.Component {
     render() {
         return (
             <div id="menu">
                 <h1 id="title">Invasion</h1>
-                <button id="start">Start</button>
+                <Button variant="contained" color="primary">
+                    Start
+                </Button>
             </div>
         );
     }
