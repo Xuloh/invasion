45267330b290ea67a3591e9169a45ee631cbbadb
Xuloh <pacome.lehoux@laposte.net>
2019-07-10 16:48:44 +0200

    added game and menu react components

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Menu from "./Menu.js";
+import Game from "./Game.js";
 
 export default class App extends React.Component {
     render() {
-        return <h1>Hello World</h1>;
+        return (
+            <div>
+                <Menu/>
+                <Game/>
+            </div>
+        );
     }
 }
---
+++ src/ui/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Menu extends React.Component {
+    render() {
+        return (
+            <div id="menu">
+                <h1 id="title">Invasion</h1>
+                <button id="start">Start</button>
+            </div>
+        );
+    }
+}
