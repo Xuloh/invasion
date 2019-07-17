98e68e42c89986b511f0bf0dd3d8654e4d0b035a
Xuloh <pacome.lehoux@laposte.net>
2019-07-17 13:34:23 +0200

    renamed Pointer to PointerComponent and moved it to the components sub directory

--- src/game/components/PointerComponent.js
+++ src/game/components/PointerComponent.js
@@ -1,12 +1,11 @@
 import Victor from "victor";
-import Entity from "./ecm/Entity.js";
+import Component from "../ecm/Component.js";
 
-export default class Pointer extends Entity {
-    constructor(position, player, distanceToPlayer) {
-        super(position, {width: 10, height: 10});
+export default class Pointer extends Component {
+    constructor(parent, player, distanceToPlayer) {
+        super(parent);
         this.player = player;
         this.distanceToPlayer = distanceToPlayer;
-        this.$container.addClass("pointer");
     }
 
     update(dt) {
@@ -14,7 +13,7 @@ export default class Pointer extends Entity {
         const mousePos = Victor.fromObject(gameState.mouse.position);
         const playerPos = this.player.position;
         const playerToMouse = mousePos.distance(playerPos);
-        this.setPosition({
+        this.parent.setPosition({
             x: playerPos.x + (mousePos.x - playerPos.x) * this.distanceToPlayer / playerToMouse,
             y: playerPos.y + (mousePos.y - playerPos.y) * this.distanceToPlayer / playerToMouse
         });
