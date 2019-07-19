9f49619d7dc5007273b512a82ad819f75dcb7791
Xuloh <pacome.lehoux@laposte.net>
2019-07-19 16:52:34 +0200

    added settings button to main menu

--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -1,6 +1,9 @@
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import PlayArrow from "@material-ui/icons/PlayArrow";
 import PropTypes from "prop-types";
 import React from "react";
+import Settings from "@material-ui/icons/Settings";
 import {Typography} from "@material-ui/core";
 
 export default class Menu extends React.Component {
@@ -13,9 +16,17 @@ export default class Menu extends React.Component {
         return (
             <div id="menu" style={{display: display}}>
                 <Typography id="title" variant="h1" gutterBottom>Invasion</Typography>
-                <Button id="start" variant="contained" color="primary" onClick={() => this.startClick()}>
-                    Start
-                </Button>
+
+                <Grid container direction="column" alignItems="center">
+                    <Button id="start" variant="contained" color="primary" size="large" onClick={() => this.startClick()}>
+                        <PlayArrow style={{marginRight: "5px"}}/>
+                        Start
+                    </Button>
+                    <Button id="settings" variant="text" color="secondary" size="medium" onClick={() => console.log("clicked settings")}>
+                        <Settings style={{marginRight: "5px"}}/>
+                        Settings
+                    </Button>
+                </Grid>
             </div>
         );
     }
