98a08b191de4d19446daa4d844fdc2e40a561532
Xuloh <pacome.lehoux@laposte.net>
2019-07-23 17:04:00 +0200

    modified MainScene to handle messages of type "spawn"

--- src/game/MainScene.js
+++ src/game/MainScene.js
@@ -17,4 +17,14 @@ export default class MainScene extends Scene {
         // this.entities.push(this.entityFactory.makeEnemy([100 * 1 / ratio, (window.innerHeight - 100) * 1 / ratio], player));
         // this.entities.push(this.entityFactory.makeEnemy([(window.innerWidth - 100) * 1 / ratio, (window.innerHeight / 2) * 1 / ratio], player));
     }
+
+    handle(message) {
+        switch(message.type) {
+            case "spawn":
+                this.entities.push(new message.args.entity(...message.args.options));
+                break;
+            default:
+                break;
+        }
+    }
 }
