e2d14fee46bde12b52f53aa46bc1d58efae66e95
Xuloh <pacome.lehoux@laposte.net>
2019-07-24 14:53:04 +0200

    changed flatColor shader to take a single color instead of one per vertex

--- src/shaders/flatColor.js
+++ src/shaders/flatColor.js
@@ -2,8 +2,8 @@ import fs from "../shaders/flatColor.fs.glsl";
 import vs from "../shaders/flatColor.vs.glsl";
 
 const infos = {
-    attributes: ["vertexPosition", "vertexColor"],
-    uniforms: ["projectionMatrix", "modelViewMatrix"]
+    attributes: ["vertexPosition"],
+    uniforms: ["projectionMatrix", "modelViewMatrix", "color"]
 };
 
 export {
--- src/shaders/flatColor.vs.glsl
+++ src/shaders/flatColor.vs.glsl
@@ -1,12 +1,12 @@
 attribute vec4 aVertexPosition;
-attribute vec4 aVertexColor;
 
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
+uniform vec4 uColor;
 
 varying lowp vec4 vColor;
 
 void main() {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    vColor = aVertexColor;
+    vColor = uColor;
 }
