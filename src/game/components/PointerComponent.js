c25a958bdaeb803432a47c66c6c77b694a51ae0a
Xuloh <pacome.lehoux@laposte.net>
2019-07-24 15:50:05 +0200

    updated PointerComponent to use new Transform2DComponent

--- src/game/components/PointerComponent.js
+++ src/game/components/PointerComponent.js
@@ -1,10 +1,12 @@
 import Component from "../ecm/Component";
+import Transform2DComponent from "./Transform2DComponent";
 import Victor from "victor";
 import {getMousePosition} from "../../events/ControlsManager";
 
 export default class PointerComponent extends Component {
     constructor(parent, player, distanceToPlayer) {
         super(parent);
+        this.transform2d = this.require(Transform2DComponent);
         this.player = player;
         this.distanceToPlayer = distanceToPlayer;
     }
@@ -15,10 +17,10 @@ export default class PointerComponent extends Component {
         const mousePos = Victor.fromObject(getMousePosition).multiply({x: 1 / ratio, y: 1 / ratio});
         const playerPos = this.player.position;
         const playerToMouse = mousePos.distance(playerPos);
-        this._parent.position = {
+        this.transform2d.position = {
             x: playerPos.x + (mousePos.x - playerPos.x) * this.distanceToPlayer / playerToMouse,
             y: playerPos.y + (mousePos.y - playerPos.y) * this.distanceToPlayer / playerToMouse
         };
-        this._parent.angle = mousePos.subtract(playerPos).angle();
+        this.transform2d.angle = mousePos.subtract(playerPos).angle();
     }
 }
