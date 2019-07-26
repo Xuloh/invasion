630a775cf15e6b24c5df198b72fea08091c2d53a
Xuloh <pacome.lehoux@laposte.net>
2019-07-26 16:31:25 +0200

    tweaked enemy

--- src/game/entities/Enemy.js
+++ src/game/entities/Enemy.js
@@ -7,7 +7,7 @@ export default class Enemy extends Entity2D {
     constructor(position, player) {
         super(position, 0.0, 1.0, "Enemy");
         this.addComponent(PhysicsComponent, 1, "enemy");
-        this.addComponent(EnemyComponent, player, 0.7, {x: 2.5, y: 2.5});
-        this.addComponent(PolygonComponent, 20, [0.65, 0.4, 0.35, 1.0], 1);
+        this.addComponent(EnemyComponent, player, 0.7, 2.5);
+        this.addComponent(PolygonComponent, 30, [0.65, 0.4, 0.35, 1.0], 1);
     }
 }
