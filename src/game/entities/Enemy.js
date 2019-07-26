79d5676e77fca0b6bd2277bd2d43cc8fe87c55f7
Xuloh <pacome.lehoux@laposte.net>
2019-07-26 16:20:30 +0200

    fixed a bug in Enemy and changed enemy color

--- src/game/entities/Enemy.js
+++ src/game/entities/Enemy.js
@@ -1,13 +1,13 @@
+import EnemyComponent from "game/components/EnemyComponent";
 import Entity2D from "game/entities/Entity2D";
 import PhysicsComponent from "game/components/PhysicsComponent";
-import EnemyComponent from "game/components/PlayerComponent";
 import PolygonComponent from "game/components/PolygonComponent";
 
 export default class Enemy extends Entity2D {
     constructor(position, player) {
         super(position, 0.0, 1.0, "Enemy");
         this.addComponent(PhysicsComponent, 1, "enemy");
-        this.addComponent(EnemyComponentd, 0.7, {x: 2.5, y: 2.5});
-        this.addComponent(PolygonComponent, 20, [0.4, 0.6, 0.4, 1.0], 1);
+        this.addComponent(EnemyComponent, player, 0.7, {x: 2.5, y: 2.5});
+        this.addComponent(PolygonComponent, 20, [0.65, 0.4, 0.35, 1.0], 1);
     }
 }
