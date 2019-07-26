a403a0046fdabbab010b3c0ef38af5d79685f0eb
Xuloh <pacome.lehoux@laposte.net>
2019-07-26 15:30:42 +0200

    added methods to define collision filter categories in PhysicsManager

--- src/game/PhysicsManager.js
+++ src/game/PhysicsManager.js
@@ -3,6 +3,10 @@ import {Engine, World} from "matter-js";
 let world = null;
 let engine = null;
 
+// categories used for collision detection
+let categoryCount = 0;
+const collisionFilters = {};
+
 function init(gravity) {
     if(gravity == null)
         gravity = {x: 0, y: 9.81};
@@ -30,8 +34,40 @@ function getWorld() {
     return world;
 }
 
+function addCategory(category) {
+    if(Object.prototype.hasOwnProperty.call(collisionFilters, category))
+        throw new Error(`Category ${category} already exists`);
+    if(categoryCount > 32)
+        throw new Error("Maximum number of categories 32 reached");
+
+    collisionFilters[category] = {
+        category: 1 << categoryCount++,
+        mask: 0
+    };
+}
+
+function setCategoryMask(category, otherCategories, remove) {
+    let mask = Object.keys(collisionFilters)
+        .filter(k => otherCategories.includes(k))
+        .reduce((acc, k) => acc | collisionFilters[k].category, 0);
+
+    if(remove) {
+        mask = ~mask;
+        collisionFilters[category].mask &= mask;
+    }
+    else
+        collisionFilters[category].mask |= mask;
+}
+
+function getCollisionFilter(category) {
+    return collisionFilters[category];
+}
+
 export {
     init,
     update,
-    getWorld
+    getWorld,
+    addCategory,
+    setCategoryMask,
+    getCollisionFilter
 };
