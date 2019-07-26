c9d7b4d09962506d5cf919a6487553c6e620c36a
Xuloh <pacome.lehoux@laposte.net>
2019-07-26 15:32:02 +0200

    modified Player, Bullet and Enemey to use the new collision filter categories

--- src/game/entities/Enemy.js
+++ src/game/entities/Enemy.js
@@ -6,7 +6,7 @@ import PolygonComponent from "game/components/PolygonComponent";
 export default class Enemy extends Entity2D {
     constructor(position, player) {
         super(position, 0.0, 1.0, "Enemy");
-        this.addComponent(PhysicsComponent, 1);
+        this.addComponent(PhysicsComponent, 1, "enemy");
         this.addComponent(EnemyComponentd, 0.7, {x: 2.5, y: 2.5});
         this.addComponent(PolygonComponent, 20, [0.4, 0.6, 0.4, 1.0], 1);
     }
--- src/game/entities/Player.js
+++ src/game/entities/Player.js
@@ -6,7 +6,7 @@ import PolygonComponent from "game/components/PolygonComponent";
 export default class Player extends Entity2D {
     constructor(position) {
         super(position, 0.0, 1.0, "Player");
-        this.addComponent(PhysicsComponent, 1);
+        this.addComponent(PhysicsComponent, 1, "player");
         this.addComponent(PolygonComponent, 30, [0.4, 0.6, 0.4, 1.0], 1);
         this.addComponent(PlayerComponent, 1, 3);
     }
