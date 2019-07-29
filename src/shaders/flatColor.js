dca3be497f2493bbd662da6f60be9d3138a36169
Xuloh <pacome.lehoux@laposte.net>
2019-07-29 16:23:25 +0200

    modified shader js file

--- src/shaders/flatColor.js
+++ src/shaders/flatColor.js
@@ -1,13 +1,7 @@
 import fs from "shaders/flatColor.fs.glsl";
 import vs from "shaders/flatColor.vs.glsl";
 
-const infos = {
-    attributes: ["vertexPosition"],
-    uniforms: ["projectionMatrix", "modelViewMatrix", "color"]
-};
-
 export {
     fs,
-    vs,
-    infos
+    vs
 };
