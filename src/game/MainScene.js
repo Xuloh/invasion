d21f000737db9d782063bdfa074cba90f2aa8537
Xuloh <pacome.lehoux@laposte.net>
2019-07-31 13:29:50 +0200

    added pointer in main scene

--- src/game/MainScene.js
+++ src/game/MainScene.js
@@ -1,5 +1,6 @@
 import EnemySpawner from "game/entities/EnemySpawner";
 import Player from "game/entities/Player";
+import Pointer from "game/entities/Pointer";
 import Scene from "game/ecm/Scene";
 
 export default class MainScene extends Scene {
@@ -8,6 +9,7 @@ export default class MainScene extends Scene {
         const player = new Player([0, 0]);
         this.entities.push(player);
         this.entities.push(new EnemySpawner(player));
+        this.entities.push(new Pointer(player));
         //this.entities.push(this.entityFactory.makeTest());
         // this.entities.push(player);
         // this.entities.push(this.entityFactory.makePointer(player));
