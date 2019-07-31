85efd9b2994dcb65f344a7acb3165e5a1a9efe44
Xuloh <pacome.lehoux@laposte.net>
2019-07-31 16:13:44 +0200

    updated EntitySpawnComponent with new Timeout class

--- src/game/components/EntitySpawnerComponent.js
+++ src/game/components/EntitySpawnerComponent.js
@@ -1,6 +1,7 @@
 import * as SceneManager from "game/SceneManager";
 import Component from "game/ecm/Component";
 import Entity2D from "game/entities/Entity2D";
+import Timeout from "util/Timeout";
 
 export default class EntitySpawnerComponent extends Component {
     constructor(parent, entity, interval, position, args) {
@@ -8,24 +9,34 @@ export default class EntitySpawnerComponent extends Component {
         if(!(entity.prototype instanceof Entity2D))
             throw new TypeError(`${entity.name} is not a subclass of Entity`);
         this.entity = entity;
+        this.spawnPosition = position;
+        this.entity = entity;
+        this.spawnArgs = args;
+        this.timeout = new Timeout(interval);
+    }
 
-        setTimeout(() => {
-            let pos;
-            if(!Object.prototype.hasOwnProperty.call(position, "width") || !Object.prototype.hasOwnProperty.call(position, "height"))
-                pos = [position.x, position.y];
-            else {
-                pos = [
-                    Math.floor(Math.random() * (position.width)) + position.x,
-                    Math.floor(Math.random() * (position.height)) + position.y
-                ];
+    update(dt) {
+        super.update(dt);
+        if(this.timeout.update(dt, true))
+            this.spawn();
+    }
+
+    spawn() {
+        let pos;
+        if(!Object.prototype.hasOwnProperty.call(this.spawnPosition, "width") || !Object.prototype.hasOwnProperty.call(this.spawnPosition, "height"))
+            pos = [this.spawnPosition.x, this.spawnPosition.y];
+        else {
+            pos = [
+                Math.floor(Math.random() * (this.spawnPosition.width)) + this.spawnPosition.x,
+                Math.floor(Math.random() * (this.spawnPosition.height)) + this.spawnPosition.y
+            ];
+        }
+        SceneManager.message({
+            type: "spawn",
+            args: {
+                entity: this.entity,
+                options: [pos, ...this.spawnArgs]
             }
-            SceneManager.message({
-                type: "spawn",
-                args: {
-                    entity: this.entity,
-                    options: [pos, ...args]
-                }
-            });
-        }, interval);
+        });
     }
 }
