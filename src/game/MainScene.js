8d47485618b8e7de44c962ec2bec58a3f41f194d
Xuloh <pacome.lehoux@laposte.net>
2019-08-01 15:03:38 +0200

    removed old comments

--- src/game/MainScene.js
+++ src/game/MainScene.js
@@ -5,17 +5,10 @@ import Scene from "game/ecm/Scene";
 
 export default class MainScene extends Scene {
     load() {
-        // const player = this.entityFactory.makePlayer();
         const player = new Player([0, 0]);
         this.entities.push(player);
         this.entities.push(new EnemySpawner(player));
         this.entities.push(new Pointer(player));
-        //this.entities.push(this.entityFactory.makeTest());
-        // this.entities.push(player);
-        // this.entities.push(this.entityFactory.makePointer(player));
-        // this.entities.push(this.entityFactory.makeEnemy([100 * 1 / ratio, 100 * 1 / ratio], player));
-        // this.entities.push(this.entityFactory.makeEnemy([100 * 1 / ratio, (window.innerHeight - 100) * 1 / ratio], player));
-        // this.entities.push(this.entityFactory.makeEnemy([(window.innerWidth - 100) * 1 / ratio, (window.innerHeight / 2) * 1 / ratio], player));
     }
 
     handle(message) {
