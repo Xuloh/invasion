4c872b7b8ae747b98b6c154a938fce70220b0a99
Xuloh <pacome.lehoux@laposte.net>
2019-08-01 13:13:45 +0200

    modified OrthographicCamera to allow changing its position

--- src/game/OrthographicCamera.js
+++ src/game/OrthographicCamera.js
@@ -2,6 +2,14 @@ import {mat4} from "gl-matrix";
 
 export default class OrthographicCamera {
     constructor(width, height, pixelRatio) {
+        this.cameraMatrix = mat4.create();
+        mat4.identity(this.cameraMatrix);
+
+        this.viewMatrix = mat4.create();
+        this.projectionMatrix = mat4.create();
+        this.viewProjectionMatrix = mat4.create();
+        this.invViewProjectionMatrix = mat4.create();
+
         this.createProjectionMatrix(width, height, pixelRatio);
         this.createViewMatrix();
         this.createViewProjectionMatrix();
@@ -11,8 +19,6 @@ export default class OrthographicCamera {
         // create projection matrix
         width = width * 1 / pixelRatio;
         height = height * 1 / pixelRatio;
-
-        this.projectionMatrix = mat4.create();
 
         mat4.ortho(
             this.projectionMatrix,
@@ -26,17 +32,21 @@ export default class OrthographicCamera {
     }
 
     createViewMatrix() {
-        this.cameraMatrix = mat4.create();
-        mat4.identity(this.cameraMatrix);
-
-        this.viewMatrix = mat4.create();
         mat4.invert(this.viewMatrix, this.cameraMatrix);
     }
 
     createViewProjectionMatrix() {
-        this.viewProjectionMatrix = mat4.create();
         mat4.multiply(this.viewProjectionMatrix, this.projectionMatrix, this.viewMatrix);
-        this.invViewProjectionMatrix = mat4.create();
         mat4.invert(this.invViewProjectionMatrix, this.viewProjectionMatrix);
+    }
+
+    set position(position) {
+        this.cameraMatrix[12] = position[0];
+        this.cameraMatrix[13] = position[1];
+        this.createViewMatrix();
+    }
+
+    get position() {
+        return [this.cameraMatrix[12], this.cameraMatrix[13]];
     }
 }
