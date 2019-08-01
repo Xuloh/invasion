fd5dea22b1905d91497748b2d5a9fa44cc65d824
Xuloh <pacome.lehoux@laposte.net>
2019-08-01 15:11:32 +0200

    changed pointer appearance

--- src/game/entities/Pointer.js
+++ src/game/entities/Pointer.js
@@ -1,11 +1,11 @@
 import Entity2D from "game/entities/Entity2D";
 import PointerComponent from "game/components/PointerComponent";
-import PolygonComponent from "game/components/PolygonComponent";
+import TriangleComponent from "game/components/TriangleComponent";
 
 export default class Pointer extends Entity2D {
     constructor(player) {
         super([0.0, 0.0], 0.0, 1.0, "Pointer");
         this.addComponent(PointerComponent, player, 1.5);
-        this.addComponent(PolygonComponent, 10, null, 0.1);
+        this.addComponent(TriangleComponent, [0.0, 0.0, 0.0, 1.0], 0.3);
     }
 }
